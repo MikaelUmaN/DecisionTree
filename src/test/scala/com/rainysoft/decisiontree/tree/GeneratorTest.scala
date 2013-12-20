f586bdc230200e00e557f34557103e8e4dff9cc0
Mikael Öhman <mikael.ohman@cinnober.com>
2013-12-20 08:40:53 +0100

    start of test

--- src/test/scala/com/rainysoft/decisiontree/tree/GeneratorTest.scala
+++ src/test/scala/com/rainysoft/decisiontree/tree/GeneratorTest.scala
@@ -9,4 +9,11 @@ class GeneratorTest extends FunSuite {
   test("Remove this test") {
     assert(true)
   }
+
+  test("Split samples on attribute") {
+    val sample1 = HashSet.apply(("attribute1", "value1"), ("attribute2", "value2"), ("attribute3", "value1"))
+    val sample2 = HashSet.apply(("attribute1", "value2"), ("attribute2", "value2"), ("attribute3", "value1"))
+    val sample3 = HashSet.apply(("attribute1", "value1"), ("attribute2", "value1"), ("attribute3", "value2"))
+
+  }
 }
